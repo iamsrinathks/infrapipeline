54e7cb0ca93e75950424756878f0abdb7a25ce81
Srinath K Srinivasan <srinathksrinivasan@c02tn1qdg8wl.home>
2022-05-04 23:37:27 +0100

    added jennis

--- instance.tf
+++ instance.tf
@@ -1,12 +1,7 @@
-resource "aws_key_pair" "aws_key" {
- key_name   = "mykey"
-}
-
-
 resource "aws_instance" "webserver" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = var.INSTANCE_TYPE
-  key_name               = aws_key_pair.aws_key.key_name
+  key_name               = "mykey"
   vpc_security_group_ids = [aws_security_group.sec_group.id]
   user_data              = data.cloudinit_config.config.rendered
   tags                   = { for k, v in var.TAGS : k => upper(v) }
