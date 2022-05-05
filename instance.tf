471070770f1cf89512a0c0b387e35992ddaf70e4
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2022-05-05 09:53:07 +0100

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,14 +1,12 @@
-data "aws_key_pair" "aws_key" {
- key_name   = "assessment_kp"
-
+data "aws_key_pair" "assessment_key" {
+  key_name = "assessment_kp"
 }
 
-
 resource "aws_instance" "webserver" {
-  ami           = var.AMIS[var.AWS_REGION]
-  instance_type = var.INSTANCE_TYPE
-  key_name               = data.aws_key_pair.aws_key.key_name
-  vpc_security_group_ids = [aws_security_group.sec_group.id]
-  user_data              = data.cloudinit_config.config.rendered
-  tags                   = { for k, v in var.TAGS : k => upper(v) }
+  ami                    = var.AMIS[var.AWS_REGION]
+  instance_type          = var.INSTANCE_TYPE
+  key_name               = data.aws_key_pair.assessment_key.key_name
+  vpc_security_group_ids = [aws_security_group.assessment_sg.id]
+  user_data              = data.cloudinit_config.assessment_config.rendered
+  tags                   = { for k, v in var.TAGS : k => lower(v) }
 }
