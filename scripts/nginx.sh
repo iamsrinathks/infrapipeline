92cc9088d3f84da0a6d41a7754f248bc100d8b64
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2022-05-05 10:47:14 +0100

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo service ufw stop
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo service ufw stop
