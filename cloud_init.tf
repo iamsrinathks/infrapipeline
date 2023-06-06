0eebf01a804049f51b62c44d834389ccf041eca0
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2023-06-06 15:26:45 +0100

    Update cloud_init.tf

--- cloud_init.tf
+++ cloud_init.tf
@@ -11,3 +11,27 @@ data "cloudinit_config" "config" {
 data "template_file" "script" {
   template = file("scripts/nginx.sh")
 }
+
+
+
+locals {
+  modified_firewall_rules = [
+    for rule in var.firewall_rules : {
+      name          = "prefix-" + (length(rule.name) > 63 ? trim(rule.name, 63) : rule.name)
+      allow         = [
+        for allow_rule in rule.allow : {
+          ports    = allow_rule.ports
+          protocol = allow_rule.protocol
+        }
+      ]
+      deny          = [
+        for deny_rule in rule.deny : {
+          ports    = deny_rule.ports
+          protocol = deny_rule.protocol
+        }
+      ]
+      direction     = rule.direction
+      source_ranges = rule.source_ranges
+    }
+  ]
+}
