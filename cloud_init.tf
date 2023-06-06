8b6a354af9d216efa3843a8d7bf78d8000542015
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2023-06-06 15:32:16 +0100

    Update cloud_init.tf

--- cloud_init.tf
+++ cloud_init.tf
@@ -18,20 +18,21 @@ locals {
   modified_firewall_rules = [
     for rule in var.firewall_rules : {
       name          = "prefix-" + (length(rule.name) > 63 ? trim(rule.name, 63) : rule.name)
-      allow         = [
+      allow         = can(rule.allow) ? [
         for allow_rule in rule.allow : {
           ports    = allow_rule.ports
           protocol = allow_rule.protocol
         }
-      ]
-      deny          = [
+      ] : null
+      deny          = can(rule.deny) ? [
         for deny_rule in rule.deny : {
           ports    = deny_rule.ports
           protocol = deny_rule.protocol
         }
-      ]
+      ] : null
       direction     = rule.direction
-      source_ranges = rule.source_ranges
+      source_ranges = can(rule.source_ranges) ? rule.source_ranges : null
     }
   ]
 }
+
