6a2b52f2ca2feb4a49816031f31fbf11734d0e91
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2023-06-12 18:18:38 +0100

    Update cloud_init.tf

--- cloud_init.tf
+++ cloud_init.tf
@@ -19,6 +19,13 @@ firewall_rules = [
 firewall_rules = [
   for rule in local.concat_firewall_rules : tomap({ for k, v in rule : substr(lower("${if v.deny != null && v.deny != \"\" then \"deny\" else if v.allow != null && v.allow != \"\" then \"allow\" else \"\"}-${local.workspace}-${local.environment_short_code}-${k}\""), 0, 63) => v })
 ]
+   
+    firewall_rules = [
+  for rule in local.concat_firewall_rules : {
+    for k, v in rule : substr(lower("${if v.deny != null && v.deny != \"\" then \"deny\" else if v.allow != null && v.allow != \"\" then \"allow\" else \"\"}-${local.workspace}-${local.environment_short_code}-${k}\""), 0, 63) => v
+  }
+]
+
 
 
 
