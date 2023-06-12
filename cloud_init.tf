61a8740e6dfc940b90ecdbfedabab21d063bb9c8
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2023-06-12 18:58:39 +0100

    Update cloud_init.tf

--- cloud_init.tf
+++ cloud_init.tf
@@ -41,6 +41,11 @@ firewall_rules = [
   for rule in local.concat_firewall_rules : tomap({ for k, v in rule : substr(lower("${${compact([can(v.deny) ? "deny" : "", can(v.allow) ? "allow" : ""]})}-${local.workspace}-${local.environment_short_code}-${k}"), 0, 63) => v })
 ]
 
+    
+    firewall_rules = [
+  for rule in local.concat_firewall_rules : tomap({ for k, v in rule : substr(lower("$${${length(v.allow) > 0 ? "allow" : "deny"}-${local.workspace}-${local.environment_short_code}-${k}"}), 0, 63) => v })
+]
+
 gcloud container clusters describe CLUSTER_NAME --zone ZONE --project PROJECT_ID --format='value(networkConfig.networkPlugin)'
 
 locals {
